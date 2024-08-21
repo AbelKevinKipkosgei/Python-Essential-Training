8369361f94eb3e6a97e6590800764fc8c7e7d6b7
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-08-21 18:49:02 +0300

    Completed control flows in Python.

---
+++ controlFlow.py
@@ -0,0 +1,26 @@
+#If/Else statements
+a = True
+b = True
+c = True
+if a:
+    print("It is true.")
+    print("Also print this.")
+    if b:
+        print("Both are true.")
+        if c:
+            print("All three are true.")
+else:
+    print("It is false")
+print("Always print this.")
+
+#For Loop
+a = [1,2,3,4,5]
+for number in a:
+    print(number)
+
+#While Loop
+x = 0
+while x < 10:
+    print(x)
+    x=x+1
+
