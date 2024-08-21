957fd422b4af60dfe19f9f880216a41bbf855add
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-08-21 12:12:18 +0300

    Learned operators in Python.

---
+++ operators.py
@@ -0,0 +1,17 @@
+#Arithmetic Operators
+add = 1 + 1
+subtract = 38 - 10
+multiply = 34 * 34
+division = 20 / 4
+modulus = 20 % 3
+print(add)
+print(subtract)
+print(multiply)
+print(division)
+print(modulus)
+
+#Comparison Operators
+print(2 == 4)
+print(5 > 3)
+print(5<=5)
+print (4>=2)
